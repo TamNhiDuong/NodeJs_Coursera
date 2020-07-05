0b9f8678e37dc42f89a6bddbd4105954838b5bb0
TamNhiDuong <yanxinyee@gmail.com>
2020-07-05 23:53:21 +0300

    Assignment 1-Express Router

--- Node_Express/index.js
+++ Node_Express/index.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-     http = require('http');
+    http = require('http');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -13,20 +13,20 @@ app.use(express.static(__dirname + '/public'));
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-const dishRouter = require('./routes/dishRouter');
-
-app.use('/dishes', dishRouter);
+//Use Router
+const dishRouter = require('./routes/promoRouter');
+app.use('/promotions', dishRouter);
 
 app.use((req, res, next) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
 });
 
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
