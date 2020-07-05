8c34b8b511eb73fe472b444dae7efcea8f38f84e
TamNhiDuong <yanxinyee@gmail.com>
2020-07-05 23:23:34 +0300

    Express Router

--- Node_Express/index.js
+++ Node_Express/index.js
@@ -1,24 +1,32 @@
-const express = require('express');
-const http = require('http');
-const morgan = require('morgan');
+const express = require('express'),
+     http = require('http');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
-app.use(morgan('dev'));
 
-app.use(express.static(__dirname+'/public'));
+const morgan = require('morgan');
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const dishRouter = require('./routes/dishRouter');
+
+app.use('/dishes', dishRouter);
 
 app.use((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    //res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
 });
 
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
