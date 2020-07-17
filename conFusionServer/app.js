031d0bf9ed42004cdf3a51032e2b0e7b7ed71635
TamNhiDuong <yanxinyee@gmail.com>
2020-07-17 23:40:20 +0300

    use Passport to authenticate- simplier way

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
@@ -47,30 +50,28 @@ app.use(session({
   store: new FileStore()
 }));
 
+//PASSPORT
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Go to those routes before authentication
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 //AUTHENTICATION
 function auth (req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-if(!req.session.user) {
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
-    return next(err);
-}
-else {
-  if (req.session.user === 'authenticated') {
-    next();
+    next(err);
   }
   else {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
+        next();
   }
 }
-}
+
 
 app.use(auth);
 
